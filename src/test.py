68f530c8dc4741af692d0da04348f374229f06e5
dwong229 <jasoc@seas.upenn.edu>
2017-07-10 18:17:18 -0400

    opencv testing

--- src/test.py
+++ src/test.py
@@ -6,28 +6,36 @@ from cv_bridge import CvBridge, CvBridgeError
 import cv2
 import numpy as np
 
-bridge = ''
+# bridge = ''
 
-def callback(data):
-    cv_data = bridge.imgmsg_to_cv2(data, "bgr8")
-    cv2.imshow("figure", cv_data)
-    cv2.waitKey(1)
+# def callback(data):
+#     cv_data = bridge.imgmsg_to_cv2(data, "bgr8")
+#     cv2.imshow("figure", cv_data)
+#     cv2.waitKey(1)
 
-def callback2(data):
-    cv_data = bridge.imgmsg_to_cv2(data, "bgr8")
-    cv2.imshow("figure2", cv_data)
-    cv2.waitKey(1)
+# def listen():
+#     cv2.namedWindow("figure")
 
-def listen():
-    cv2.namedWindow("figure")
-    cv2.namedWindow("figure2")
+#     rospy.init_node('listen', anonymous=True)
+#     rospy.Subscriber("/camera/image_raw", Image, callback)
+#     # rospy.Subscriber("/camera/image_mono", Image, callback)
+#     # rospy.Rate(10)
+#     rospy.spin()
 
-    rospy.init_node('listen', anonymous=True)
-    rospy.Subscriber("/camera/image_raw", Image, callback)
-    rospy.Subscriber("/camera/image_mono", Image, callback2)
-    # rospy.Rate(10)
-    rospy.spin()
+# if __name__ == "__main__":
+#     bridge = CvBridge()
+#     listen()
 
 if __name__ == "__main__":
-    bridge = CvBridge()
-    listen()
+    img = cv2.imread(
+        "/home/denise/catkin_ws/src/siskiyou/images/colony_sample.JPG")
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.namedWindow("figure")
+    img_resized = cv2.resize(img,(0,0),fx=0.25,fy=0.25)
+    im_h, im_w, _ = img_resized.shape
+    cv2.imshow("figure", img_resized)
+    # cv2.imshow("figure2", cv2.Canny(img_resized,120,130))
+    # cv2.moveWindow("figure", 2000, 0)
+    # cv2.moveWindow("figure2", 2000,im_h)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
