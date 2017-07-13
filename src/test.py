3f2a2f95f898456e079888a0c17b1d64d8294d75
dwong229 <jasoc@seas.upenn.edu>
2017-07-13 16:20:48 -0400

    outline detection

--- src/test.py
+++ src/test.py
@@ -29,21 +29,21 @@ import time
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(
-        "/home/Documents/Jason/media/pipette_focus.avi")
+        "/home/denise/Documents/Jason/media/pipette_focus.avi")
 
     frame = cv2.imread(
-        "/home/Documents/Jason/media/pipette_frame.png")
+        "/home/denise/Documents/Jason/media/pipette_frame.png")
 
     if cap.isOpened():
         v_w = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH))
         v_h = int(cap.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT))
 
-        offset = 80
+        offset = 50
         cv2.namedWindow("figure")
         cv2.namedWindow("figure2")
         cv2.moveWindow("figure", offset, offset)
         cv2.moveWindow("figure2", offset+v_w, offset)
-        
+
         while True:
             ret, frame = cap.read()
             if ret is not True:
@@ -53,19 +53,31 @@ if __name__ == "__main__":
 
             # frame = cv2.medianBlur(frame, 5)
             
-            # [ignore >330%, ignore <45%, ignore >30%]
+            # [120-165, 0-255, 0-255*0.38]
             lower = np.array([120, 0, 0])
-            upper = np.array([165, 255, int(255*0.35)])
+            upper = np.array([170, int(255*0.50), int(255*0.36)])
             mask = cv2.inRange(frame_hsv, lower, upper)
 
             # mask_gauss = cv2.GaussianBlur(mask, (5,5), 0)
             mask_median = cv2.medianBlur(mask, 5)
+            mask_close = cv2.morphologyEx(mask_median, 
+                cv2.MORPH_CLOSE, np.ones((20,20),np.uint8))
 
-            cv2.imshow("figure", frame)
-            cv2.imshow("figure2", mask_median)
+            edges = cv2.Canny(mask_close, 100, 200)
+            line = np.zeros((v_h,v_w,3), np.uint8)
+            line[:,:,1] = 255
+            outline = cv2.bitwise_and(line, line, mask=edges)
+            frame_prep = cv2.bitwise_and(frame, frame, 
+                mask=cv2.bitwise_not(edges))
+            overlay = cv2.add(frame_prep, outline)
+            # overlay = cv2.add(frame, overlay)
 
+            cv2.imshow("figure", overlay)
+            cv2.imshow("figure2", mask_close)
             cv2.waitKey(1)
         
         print "Finished!"
         cv2.waitKey(0)
         cv2.destroyAllWindows()
+    else:
+        print "Problem with video"
